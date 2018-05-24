3a7166287c3515edb44cb33e890a99527f32ddc5
Zaplangoc <39605543+Zaplangoc@users.noreply.github.com>
2018-05-25 02:30:57 +0300

    Add files via upload

---
+++ Robot_Kol.m
@@ -0,0 +1,12 @@
+
+L_1 = 20;
+L_2 = 50;
+L_3 = 40;
+
+L(1) = Link([0 L_1 0 pi/2]);
+L(2) = Link([0 0 L_2 0]);
+L(3) = Link([0 0 L_3 0]);
+
+Robot = SerialLink(L);
+Robot.name = 'Robot';
+Robot.teach
